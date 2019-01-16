de3d0eee5330d6c3b1ce58698e41e1e694971520
paulcp303 <467457110@qq.com>
2019-01-16 22:28:11 +0800

    1.添加IP帮助类、json返回帮助类 2.添加添加访问记录接口

---
+++ Helper/ReturnHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Helper
+{
+    public class ReturnHelper
+    {
+        public int code { get; set; }
+        public DataTable data { get; set; }
+        public int totals { get; set; }
+        public string msg { get; set; }
+
+        /// <summary>
+        /// json返回帮助类
+        /// </summary>
+        /// <param name="code">返回码（200正常，500出错，300缺少参数,400数据库执行问题）</param>
+        /// <param name="data"></param>
+        /// <param name="totals"></param>
+        /// <param name="msg"></param>
+        public ReturnHelper(int code, DataTable data, int totals, string msg)
+        {
+            this.code = code;
+            this.data = data;
+            this.totals = totals;
+            this.msg = msg;
+        }
+    }
+}
--- WebAPI/Controllers/VisitsController.cs
+++ WebAPI/Controllers/VisitsController.cs
@@ -1,15 +1,78 @@
-﻿using System;
+﻿using DAL;
+using Helper;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Web.Http;
 
 namespace WebAPI.Controllers
 {
     public class VisitsController : ApiController
     {
+        /// <summary>
+        /// 返回json格式数据
+        /// </summary>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        public HttpResponseMessage ReturnJson(string content)
+        {
+            HttpResponseMessage result = new HttpResponseMessage { Content = new StringContent(content, Encoding.GetEncoding("UTF-8"), "application/json") };
+            return result;
+        }
 
+        /// <summary>
+        /// 添加访问记录
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public HttpResponseMessage AddVisitRecord(Visits obj)
+        {
+            ReturnHelper rh = new ReturnHelper(200, null, 0, "");
 
+            try
+            {
+                obj.Visitid = Guid.NewGuid().ToString("N");
+                obj.Visittime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                obj.Ip = IPHelper.GetHostIP();
+                obj.Address = IPHelper.GetHostAddress(obj.Ip);
+                string selectsql = string.Format(" select Times from Visits where IP='{0}'",obj.Ip);
+                string times = SqlHelper.ShowData(selectsql, "Times", SqlHelper.CreateConn());
+                if (!string.IsNullOrEmpty(times))
+                {
+                    obj.Times = Convert.ToInt32(times) + 1;//访问次数+1
+                }
+                else
+                {
+                    obj.Times =  1;//第一次访问
+                }
+                int i = obj.Insert();
+                if (i > 0)
+                {
+                    rh.msg = "添加记录成功";
+                    rh.totals = i;
+                }
+                else
+                {
+                    rh.msg = "添加记录失败";
+                    rh.code = 400;
+                }
+            }
+            catch (Exception e)
+            {
+                rh.code = 500;
+                rh.msg = "服务器错误，请通知管理员";
+            }
+
+            return ReturnJson(JsonConvert.SerializeObject(rh));
+        }
+
+        public HttpResponseMessage GetVisitsPage()
+        {
+
+        }
     }
 }
